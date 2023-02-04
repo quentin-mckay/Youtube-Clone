a8c1bb99098dd6598696bff1a88fffe580f0aaae
Quentin McKay <quentinmckay1@gmail.com>
2023-02-04 18:21:06 +1100

    added Sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,29 @@
-import { Stack } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
-import { logo } from '../utils/constants'
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
-  return (
-	  <Stack 
-      direction='row' 
-      alignItems='center' 
-      p={2} 
-      sx={{position: 'sticky', background: '#222', top: 0, justifyContent: 'space-between'}} 
-    >
-      {/* not exactly sure why we need the flex and alignItems. veeery small difference */}
-      <Link to='/' style={{ display: 'flex', alignItems: 'center'}}>
-        <img src={logo} alt="logo" height={45}/>
-      </Link>
-    </Stack>
-  )
-}
+    return (
+        <Stack
+            direction="row"
+            alignItems="center"
+            p={2}
+            sx={{
+                position: "sticky",
+                background: "#222",
+                top: 0,
+                justifyContent: "space-between",
+            }}
+        >
+            <Link to='/' style={{ display: 'flex', alignItems: 'center'}}>
+            {/* <Link to="/"> */}
+                <img src={logo} alt="logo" height={45} />
+            </Link>
+			<SearchBar />
+        </Stack>
+    );
+};
 
-export default Navbar
+export default Navbar;
